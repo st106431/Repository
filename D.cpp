31f1aec81b5eca7f6d340d44a6bf9918a2b63cfb
st106431 <113363606+st106431@users.noreply.github.com>
2023-02-24 22:38:57 +0300

    Update D.cpp

--- D.cpp
+++ D.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+#include <iostream>
+#include <random>
 using namespace std;
 #define _CRT_SECURE_NO_WARNINGS
 
@@ -8,7 +9,12 @@ int mas_y[13]{ 0 };
 int tab[13][13]{ 0 };
 int buff[3]{ 0 };
 
-int interpolation(int mas_x[13], int mas_y[13], int number, int x)
+int polynom(int h)
+{
+	return (h * h + 1000000006) % m;
+}
+
+long long interpolation(int mas_x[13], int mas_y[13], int number, int x)
 {
 	for (int i = 0; i < number; i++)
 	{
@@ -18,46 +24,60 @@ int interpolation(int mas_x[13], int mas_y[13], int number, int x)
 	{
 		for (int k = 0; k < number - j - 1; k++)
 		{
-			tab[k][j + 1] = (tab[k + 1][j] - tab[k][j]) % m;
+			tab[k][j + 1] = tab[k + 1][j] - tab[k][j];
 		}
 	}
-	int s = tab[0][0];
+	long long s = tab[0][0];
 	int y = (x - mas_x[0]) / (mas_x[1] - mas_x[0]);
 	int ras = (x - mas_x[0]) / (mas_x[1] - mas_x[0]);
 	for (int j = 1; j < number - 1; j++)
 	{
 		y /= j;
-		s += (tab[0][j] * y) % m;
-		y *= (ras - j) % m;
+		s += (tab[0][j] * static_cast<long long>(y)) % m;
+		y *= ras - j;
 	}
-	return s;
+	cout << s % m << endl;
+	return s % m;
 }
 
 int main()
 {
+	mt19937 rng(123456);
+	uniform_int_distribution <int> random(1, 100000);
+	int a = random(rng);
+	int d = random(rng);
 	for (int i = 0; i < 3; i++)
 	{
-		mas_x[i] = i;
+		if (i == 0)
+		{
+			mas_x[i] = a;
+		}
+		else
+		{
+			mas_x[i] = (mas_x[i - 1] + d) % m;
+		}
 		cout << "ask "<< mas_x[i] << endl;
+		cout << polynom(mas_x[i]) << endl;
 		fflush(stdout);
 		cin >> mas_y[i];
 		buff[i] = mas_y[i];
 	}
-	if (interpolation(mas_x, mas_y, 1, 1) == buff[1] && interpolation(mas_x, mas_y, 1, 2) == buff[2])
+	if (interpolation(mas_x, mas_y, 1, mas_x[1]) == buff[1] && interpolation(mas_x, mas_y, 1, mas_x[2]) == buff[2])
 	{
 		cout << "degree " << 0 << endl;
 		return 0;
 	}
 	for (int i = 3; i < 13; i++)
 	{
-		mas_x[i] = i;
+		mas_x[i] = (mas_x[i - 1] + d) % m;
 		cout << "ask " << mas_x[i] << endl;
+		cout << polynom(mas_x[i]) << endl;
 		fflush(stdout);
 		cin >> mas_y[i];
 		buff[0] = mas_y[i - 2];
 		buff[1] = mas_y[i - 1];
 		buff[2] = mas_y[i];
-		if (interpolation(mas_x, mas_y, i, i - 1) == buff[1] && interpolation(mas_x, mas_y, i, i) == buff[2])
+		if (interpolation(mas_x, mas_y, i, mas_x[i - 1]) == buff[1] && interpolation(mas_x, mas_y, i, mas_x[i]) == buff[2])
 		{
 			cout << "degree "<< i - 2 << endl;
 			return 0;
