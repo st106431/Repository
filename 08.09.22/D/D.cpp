224e2b110383ac310c82e072f2551f31251c175f
st106431 <113363606+st106431@users.noreply.github.com>
2023-03-01 01:00:58 +0300

    08.09.22-E

--- 08.09.22/D/D.cpp
+++ 08.09.22/D/D.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
-
+	int l, r, d, c = 0;
+	scanf_s("%d %d %d", &l, &r, &d);
+	for (int i = l / d; i < r / d + 1; i++)
+	{
+		if (l <= d * i && d * i <= r)
+		{
+			printf("%d\n", d * i);
+		}
+	}
 }
---
+++ 08.09.22/E/E.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	char s[100]{' '};
+	int a[100]{ 0 };
+	char c;
+	int k = 0, h = 0, w = 0;
+    gets_s(s);
+	while (s[w] != ' ' || w != 99)
+	{
+		w++;
+	}
+	c = s[0];
+	for (int i = 0; i < w; i++)
+	{
+		for (int j = 0; j < w; j++)
+		{
+			if (s[j] == s[i])
+			{
+				a[i]++;
+			}
+		}
+	}
+	for (int i = 0; i < w; i++)
+	{
+		if (a[i] > k)
+		{
+			k = a[i];
+			h = i;
+		}
+	}
+	printf("%c\n", s[h]);
+}
