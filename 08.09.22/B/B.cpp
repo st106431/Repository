44ee7994678049e45b9c0cca33ecad8f3cbc99ed
st106431 <113363606+st106431@users.noreply.github.com>
2023-02-28 23:54:16 +0300

    08.09.22-03

--- 08.09.22/B/B.cpp
+++ 08.09.22/B/B.cpp
@@ -2,5 +2,42 @@
 
 int main()
 {
-	scanf_s("%d %d %d", &a, &b, &d);
+	long long n;
+	long long p = 1;
+	scanf_s("%lld", &n);
+	if (n == 1 || n == -1)
+	{
+		printf("%lld\n", 1);
+		return 0;
+	}
+	if (n >= 0)
+	{
+		while (p <= 1000000000)
+		{
+			printf("%lld\n", p);
+			if (p * n > 1000000000)
+			{
+				return 0;
+			}
+			else
+			{
+				p *= n;
+			}
+		}
+	}
+	else
+	{
+		while (p <= 1000000000)
+		{
+			printf("%lld\n", p);
+			if (p * n * n > 1000000000)
+			{
+				return 0;
+			}
+			else
+			{
+				p *= n * n;
+			}
+		}
+	}
 }
---
+++ 08.09.22/С/С.cpp
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+int k[100], n, a, s = 0, numb = 0, pr = 1;
+
+int t (int w, int c)
+{
+	int p = 0, g = 1, l = 0;
+	do 
+	{
+		k[l++] = w % c;
+		w /= c;
+	} while (w != 0);
+	for (int i = 0; i < l; i++) 
+	{
+		p += k[i] * g;
+		g *= 10;
+	}
+	return p;
+}
+
+int main()
+{
+	scanf_s("%d", &n);
+	a = n;
+	while (a != 0)
+	{
+		a /= 10;
+		numb++;
+	}
+	for (int i = 0; i < numb; i++)
+	{
+		s += (n % 10) * pr;
+		pr *= 5;
+		n /= 10;
+	}
+	//printf("%d\n", s);
+	a = t(s, 7), s = 0;
+	while (a > 0)
+	{
+		s += a % 10;
+		a /= 10;
+	}
+	printf("%d\n", t(s,3));
+}
