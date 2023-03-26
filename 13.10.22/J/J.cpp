db8825d211e5b05108b6ae777c958a15e3cad57f
st106431 <113363606+st106431@users.noreply.github.com>
2023-03-26 20:02:05 +0300

    768

--- 13.10.22/J/J.cpp
+++ 13.10.22/J/J.cpp
@@ -4,7 +4,7 @@
 #include <random>
 using namespace std;
 
-const long double C = sqrt(2 * acos(-1.0));
+const long double C = sqrt((long double)2 * acos((long double) - 1.0));
 
 long double func(long double x)
 {
@@ -15,19 +15,22 @@ long double integral(long double a, long double b, long long n)
 {
 	long double sum;
 	long double d = (b - a) / (long double)n;
-	long double x = a + d;
+	long double x = a;
 	sum = 0.0;
-	mt19937 rng(1374883);
 	while (x <= b)
 	{
-		uniform_int_distribution <long long> random(1, 1000000);
-		sum += func(x - (d / (long double)random(rng))) * d;
-		x += d;
-		if (x > b)
+		if(x == a || x == b)
 		{
-			break;
+			sum += func(x) / 2;
+			x += d;
+		}
+		else
+		{
+			sum += func(x - (d / 2));
+			x += d;
 		}
 	}
+	sum *= d;
 	return sum;
 }
 
@@ -36,6 +39,6 @@ int main()
 	long double p, q, x_0;
 	cin >> p >> q;
 	x_0 = p / q;
-	long double res = integral((long double) - 100.1, (long double)-1.0, (long double)10000000) + integral((long double)-1.0, (long double)0.0, (long double)10000000) + integral((long double)0.0, (long double)x_0 / 2.0, (long double)10000000) + integral((long double)x_0 / 2.0, x_0, (long double)10000000);
-	cout << fixed << setprecision(12) << res;
+	long double res = integral((long double)-20, (long double)x_0, (long long)10000000);
+	cout << fixed << setprecision(13) << res;
 }
