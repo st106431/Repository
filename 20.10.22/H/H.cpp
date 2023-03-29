ae127d2189c2ac5bd1c9c37963f7091a6d7b0c40
st106431 <113363606+st106431@users.noreply.github.com>
2023-03-30 00:26:01 +0300

    6778

--- 20.10.22/H/H.cpp
+++ 20.10.22/H/H.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <utility>
 using namespace std;
 
 void line(long double x_1, long double y_1, long double x_2, long double y_2, long double* a, long double* b, long double* c, int i)
@@ -8,49 +9,71 @@ void line(long double x_1, long double y_1, long double x_2, long double y_2, lo
 	c[i] = -(a[i] * x_1 + b[i] * y_1);
 }
 
-int main()
+bool in(long double x, long double y, long double x_1, long double y_1, long double x_2, long double y_2)
 {
-	long double* a = new long double[3];
-	long double* b = new long double[3];
-	long double* c = new long double[3];
-	long double x_1, y_1, x_2, y_2, x_3, y_3, x, y;
-	cin >> x_1 >> y_1 >> x_2 >> y_2 >> x_3 >> y_3 >> x >> y;
-	line(x_1, y_1, x_2, y_2, a, b, c, 0);
-	line(x_1, y_1, x_3, y_3, a, b, c, 1);
-	line(x_2, y_2, x_3, y_3, a, b, c, 2);
-	int count = 0, k = 0;
-	if (a[0] * x_3 + b[0] * y_3 + c[0] > 0)
+	long double ai = y_2 - y_1;
+	long double bi = x_1 - x_2;
+	long double ci = -(ai * x_1 + bi * y_1);
+	if (x_1 == x_2 && y_1 == y_2)
 	{
-		if (a[0] * x + b[0] * y + c[0] > 0)
+		if (x == x_1 && y == y_1)
 		{
-			count++;
+			return 1;
 		}
 		else
 		{
-			if (a[0] * x + b[0] * y + c[0] < 0)
+			return 0;
+		}
+	}
+	if (ai * x + bi * y + ci == 0)
+	{
+		if (x_1 == x_2)
+		{
+			if (y >= min(y_1, y_2) && y <= max(y_1, y_2))
 			{
-				cout << "OUTSIDE";
-				return 0;
+				return 1;
 			}
 			else
 			{
-				k = 1;
-				count++;
+				return 0;
 			}
+		}
+		if (x >= min(x_1, x_2) && x <= max(x_1, x_2))
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
 		}
 	}
 	else
 	{
-			if (a[0] * x + b[0] * y + c[0] > 0)
+		return 0;
+	}
+}
+
+pair <int, int> test(long double a, long double b, long double c, long double x, long double y, long double x_0, long double y_0, int f, int g)
+{
+	int count = f;
+	int k = g;
+	if (a * x_0 + b * y_0 + c == 0)
+	{
+		k = -1;
+	}
+	else
+	{
+		if (a * x_0 + b * y_0 + c > 0)
+		{
+			if (a * x + b * y + c > 0)
 			{
 				count++;
 			}
 			else
 			{
-				if (a[0] * x + b[0] * y + c[0] < 0)
+				if (a * x + b * y + c < 0)
 				{
-					cout << "OUTSIDE";
-					return 0;
+					;
 				}
 				else
 				{
@@ -58,5 +81,149 @@ int main()
 					count++;
 				}
 			}
+		}
+		else
+		{
+			if (a * x + b * y + c < 0)
+			{
+				count++;
+			}
+			else
+			{
+				if (a * x + b * y + c > 0)
+				{
+					;
+				}
+				else
+				{
+					k = 1;
+					count++;
+				}
+			}
+		}
+	}
+	return make_pair(count, k);
+}
+
+int main()
+{
+	long double* a = new long double[3];
+	long double* b = new long double[3];
+	long double* c = new long double[3];
+	long double x_1, y_1, x_2, y_2, x_3, y_3, x, y;
+	cin >> x_1 >> y_1 >> x_2 >> y_2 >> x_3 >> y_3 >> x >> y;
+	pair <long double, long double> p_1 = make_pair(x_1, y_1);
+	pair <long double, long double> p_2 = make_pair(x_2, y_2);
+	pair <long double, long double> p_3 = make_pair(x_3, y_3);
+	if (p_1 == p_2)
+	{
+		if (p_2 == p_3)
+		{
+			if (x == x_1 && y == y_1)
+			{
+				cout << "BORDER";
+				return 0;
+			}
+			else
+			{
+				cout << "OUTSIDE";
+				return 0;
+			}
+		}
+		else
+		{
+			if (in(x, y, x_2, y_2, x_3, y_3))
+			{
+				cout << "BORDER";
+				return 0;
+			}
+			else
+			{
+				cout << "OUTSIDE";
+				return 0;
+			}
+		}
+	}
+	else
+	{
+		if (p_2 == p_3)
+		{
+			if (in(x, y, x_1, y_1, x_2, y_2))
+			{
+				cout << "BORDER";
+				return 0;
+			}
+			else
+			{
+				cout << "OUTSIDE";
+				return 0;
+			}
+		}
+		else
+		{
+			if (p_1 == p_3)
+			{
+				if (in(x, y, x_1, y_1, x_2, y_2))
+				{
+					cout << "BORDER";
+					return 0;
+				}
+				else
+				{
+					cout << "OUTSIDE";
+					return 0;
+				}
+			}
+		}
+	}
+	line(x_1, y_1, x_2, y_2, a, b, c, 0);
+	line(x_1, y_1, x_3, y_3, a, b, c, 1);
+	line(x_2, y_2, x_3, y_3, a, b, c, 2);
+	pair <int, int> p = test(a[0], b[0], c[0], x, y, x_3, y_3, 0, 0);
+	p = test(a[1], b[1], c[1], x, y, x_2, y_2, p.first, p.second);
+	p = test(a[2], b[2], c[2], x, y, x_1, y_1, p.first, p.second);
+	int count = p.first;
+	int k = p.second;
+	//cout << count << " " << k << endl;
+	if (k == -1)
+	{
+		if (in(x, y, x_1, y_1, x_2, y_2))
+		{
+			cout << "BORDER";
+			return 0;
+		}
+		if (in(x, y, x_1, y_1, x_3, y_3))
+		{
+			cout << "BORDER";
+			return 0;
+		}
+		if (in(x, y, x_2, y_2, x_3, y_3))
+		{
+			cout << "BORDER";
+			return 0;
+		}
+		cout << "OUTSIDE";
+		return 0;
+	}
+	else
+	{
+		if (count == 3)
+		{
+			if (k == 1)
+			{
+				cout << "BORDER";
+				return 0;
+			}
+			else
+			{
+				cout << "INSIDE";
+				return 0;
+			}
+		}
+		else
+		{
+			cout << "OUTSIDE";
+			return 0;
+		}
 	}
 }
---
+++ 20.10.22/J/J.cpp
@@ -0,0 +1,185 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+void test(long double* x_1, long double* y_1, long double* x_2, long double* y_2, long double* x_3, long double* y_3, int i)
+{
+	pair <long double, long double> p_1 = make_pair(x_1[i], y_1[i]);
+	pair <long double, long double> p_2 = make_pair(x_2[i], y_2[i]);
+	pair <long double, long double> p_3 = make_pair(x_3[i], y_3[i]);
+	pair <long double, long double> p_4 = make_pair(x_1[i + 1], y_1[i + 1]);
+	pair <long double, long double> p_5 = make_pair(x_2[i + 1], y_2[i + 1]);
+	pair <long double, long double> p_6 = make_pair(x_3[i + 1], y_3[i + 1]);
+	if ((abs(y_1[i] - y_2[i]) < pow(10.0, -10) && abs(x_1[i] - x_2[i]) < pow(10.0, -10)) && (abs(x_1[i] - x_3[i]) < pow(10.0, -10) && abs(y_1[i] - y_3[i]) < pow(10.0, -10)))
+	{
+		if (p_4 == p_5 && p_4 == p_6)
+		{
+			cout << "YES" << endl;
+			return;
+		}
+		else
+		{
+			cout << "NO" << endl;
+			return;
+		}
+	}
+	int count_1 = 0, count_2 = 0;
+	if ((p_1 == p_2) || (p_2 == p_3) || (p_1 == p_3))
+	{
+		count_1 = 2;
+	}
+	if ((p_4 == p_5) || (p_4 == p_6) || (p_5 == p_6))
+	{
+		count_2 = 2;
+	}
+	if (count_1 == 2 && count_2 == 2)
+	{
+		cout << "YES" << endl;
+		return;
+	}
+	if (count_1 == 2 || count_2 == 2)
+	{
+		cout << "NO" << endl;
+		return;
+	}
+	long double d_12a = sqrt((x_1[i] - x_2[i]) * (x_1[i] - x_2[i]) + (y_1[i] - y_2[i]) * (y_1[i] - y_2[i]));
+	long double d_23a = sqrt((x_2[i] - x_3[i]) * (x_2[i] - x_3[i]) + (y_2[i] - y_3[i]) * (y_2[i] - y_3[i]));
+	long double d_13a = sqrt((x_1[i] - x_3[i]) * (x_1[i] - x_3[i]) + (y_1[i] - y_3[i]) * (y_1[i] - y_3[i]));
+	long double d_12b = sqrt((x_1[i + 1] - x_2[i + 1]) * (x_1[i + 1] - x_2[i + 1]) + (y_1[i + 1] - y_2[i + 1]) * (y_1[i + 1] - y_2[i + 1]));
+	long double d_23b = sqrt((x_2[i + 1] - x_3[i + 1]) * (x_2[i + 1] - x_3[i + 1]) + (y_2[i + 1] - y_3[i + 1]) * (y_2[i + 1] - y_3[i + 1]));
+	long double d_13b = sqrt((x_1[i + 1] - x_3[i + 1]) * (x_1[i + 1] - x_3[i + 1]) + (y_1[i + 1] - y_3[i + 1]) * (y_1[i + 1] - y_3[i + 1]));
+	long double k_1, k_2, k_3;
+	if (abs(d_12b) < pow(10.0, -10))
+	{
+		if (abs(d_12a) < pow(10.0, -10))
+		{
+			k_1 = -1;
+		}
+		else
+		{
+			cout << "NO" << endl;
+			return;
+		}
+	}
+	else
+	{
+		k_1 = d_12a / d_12b;
+	}
+	if (abs(d_23b) < pow(10.0, -10))
+	{
+		if (abs(d_23a) < pow(10.0, -10))
+		{
+			k_2 = -1;
+		}
+		else
+		{
+			cout << "NO" << endl;
+			return;
+		}
+	}
+	else
+	{
+		k_2 = d_23a / d_23b;
+	}
+	if (abs(d_13b) < pow(10.0, -10))
+	{
+		if (abs(d_13a) < pow(10.0, -10))
+		{
+			k_3 = -1;
+		}
+		else
+		{
+			cout << "NO" << endl;
+			return;
+		}
+	}
+	else
+	{
+		k_3 = d_13a / d_13b;
+	}
+	if (abs(k_1 + 1) > pow(10.0, -10))
+	{
+		if (abs(k_2 + 1) > pow(10.0, -10))
+		{
+			if (abs(k_3 + 1) > pow(10.0, -10))
+			{
+				if (abs(k_1 - k_2) < pow(10.0, -10) && abs(k_1 - k_3) < pow(10.0, -10))
+				{
+					cout << "YES" << endl;
+				}
+				else
+				{
+					cout << "NO" << endl;
+				}
+			}
+			else
+			{
+				if (abs(k_1  - k_2) < pow(10.0, -10))
+				{
+					cout << "YES" << endl;
+				}
+				else
+				{
+					cout << "NO" << endl;
+				}
+			}
+		}
+		else
+		{
+			if (abs(k_3 + 1) > pow(10.0, -10))
+			{
+				if (abs(k_1 - k_3) < pow(10.0, -10))
+				{
+					cout << "YES" << endl;
+				}
+				else
+				{
+					cout << "NO" << endl;
+				}
+			}
+			else
+			{
+				cout << "YES" << endl;
+			}
+		}
+	}
+	else
+	{
+		if (abs(k_2 + 1) > pow(10.0, -10))
+		{
+			if (abs(k_3 + 1) > pow(10.0, -10))
+			{
+				if (abs(k_2 - k_3) < pow(10.0, -10))
+				{
+					cout << "YES" << endl;
+				}
+				else
+				{
+					cout << "NO" << endl;
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	int c;
+	cin >> c;
+	long double* x_1 = new long double[2 * c];
+	long double* y_1 = new long double[2 * c];
+	long double* x_2 = new long double[2 * c];
+	long double* y_2 = new long double[2 * c];
+	long double* x_3 = new long double[2 * c];
+	long double* y_3 = new long double[2 * c];
+	for (int i = 0; i < c; i++)
+	{
+		cin >> x_1[2 * i] >> y_1[2 * i] >> x_2[2 * i] >> y_2[2 * i] >> x_3[2 * i] >> y_3[2 * i];
+		cin >> x_1[2 * i + 1] >> y_1[2 * i + 1] >> x_2[2 * i + 1] >> y_2[2 * i + 1] >> x_3[2 * i + 1] >> y_3[2 * i + 1];
+	}
+	for (int i = 0; i < c; i++)
+	{
+		test(x_1, y_1, x_2, y_2, x_3, y_3, 2 * i);
+	}
+}
