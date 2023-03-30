6dc53436f8bedabb25af2a05cb2fdce04689ea3a
st106431 <113363606+st106431@users.noreply.github.com>
2023-03-30 13:57:26 +0300

    774

---
+++ 03.11.22/A/A.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long n;
+	cin >> n;
+	long long* s = new long long[n + 1];
+	s[0] = 0;
+	s[1] = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		s[i] = s[i - 2] + s[i - 1];
+	}
+	cout << s[n];
+}
---
+++ 03.11.22/B/B.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int f(int* s, int k)
+{
+	if (k == 0)
+	{
+		return 0;
+	}
+	if (k == 1)
+	{
+		return s[1];
+	}
+	return s[k] + max(f(s, k - 1), f(s, k - 2));
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int n;
+	cin >> n;
+	int* s = new int[n + 1];
+	int* res = new int[n + 1];
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> s[i];
+	}
+	res[0] = 0;
+	res[1] = s[1];
+	for (int i = 2; i <= n; i++)
+	{
+		res[i] = s[i] + max(res[i - 1], res[i - 2]);
+	}
+	cout << res[n];
+}
